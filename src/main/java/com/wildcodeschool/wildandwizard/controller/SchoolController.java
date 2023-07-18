3a6236de8abfda13d1aadfc85bef439522c2c8b2
SILVERIO Olivier (SNCF RESEAU / EIC AUVERGNE NIVERNAIS / SIEGE UO SUD AUV) <8011656x@commun.ad.sncf.fr>
2023-07-18 15:24:11 +0200

    up

--- src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
@@ -1,6 +1,12 @@
 package com.wildcodeschool.wildandwizard.controller;
 
 import com.wildcodeschool.wildandwizard.entity.School;
+
+import com.wildcodeschool.wildandwizard.repository.SchoolRepository;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +18,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class SchoolController {
 
     // TODO : get school repository by dependency injection
+    @Autowired
+    private SchoolRepository schoolRepository;
 
     @GetMapping("/schools")
     public String getAll(Model model) {
 
         // TODO : find all schools
+        model.addAttribute("schools", schoolRepository.findAll());
 
         return "schools";
     }
@@ -26,6 +35,14 @@ public class SchoolController {
                             @RequestParam(required = false) Long id) {
 
         // TODO : find one school by id
+        School school = new School();
+        if (id != null) {
+            Optional<School> optionalSchool = schoolRepository.findById(id);
+            if (optionalSchool.isPresent()) {
+                school = optionalSchool.get();
+            }
+        }
+        model.addAttribute("school", school);
 
         return "school";
     }
@@ -34,6 +51,7 @@ public class SchoolController {
     public String postSchool(@ModelAttribute School school) {
 
         // TODO : create or update a school
+        schoolRepository.save(school);
 
         return "redirect:/schools";
     }
@@ -42,6 +60,7 @@ public class SchoolController {
     public String deleteSchool(@RequestParam Long id) {
 
         // TODO : delete a school
+        schoolRepository.deleteById(id);
 
         return "redirect:/schools";
     }
